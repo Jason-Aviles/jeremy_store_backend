16cfdfb3baea255c602300857e5b174173794b46
jason aviles <42799618+Jason-Aviles@users.noreply.github.com>
2022-08-24 20:35:40 -0400

    update

--- charge.js
+++ charge.js
@@ -1,14 +1,14 @@
-const Stripe = require("stripe"
-)
-const request = require('request');
+const Stripe = require("stripe");
+const request = require("request");
 
-const { endpoint, masterKey, port } = require('./config');
+const { endpoint, masterKey, port } = require("./config");
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
 
-const stripe = new Stripe(masterKey)
-
+const stripe = new Stripe(masterKey);
+const fetch = (...args) =>
+  import("node-fetch").then(({ default: fetch }) => fetch(...args));
 require("dotenv").config();
 
 const server = express();
@@ -21,74 +21,55 @@ server.use(express.json());
 server.use(cors());
 // server.options("*", cors());
 
+server.post("/api/charge", async (req, res) => {
+  const { id, amount } = req.body;
+  console.log(amount, "amount");
 
-server.post('/api/charge',async (req,res)=>{ 
-  
-  const {id ,amount} = req.body;
-console.log(amount,"amount")
+  try {
+    const payment = await stripe.paymentIntents.create({
+      amount,
 
-try {
-  const payment = await stripe.paymentIntents.create({
-    amount,
-    
-    currency:"USD",
-    description:"Forever Immortal",
-    payment_method:id,confirm:true
-  })
-console.log(payment)
-  return res.status(200).json({confirm:"done"})
-} catch (error) {
-  console.log(error)
-  return res.status(400).json({message:error.message})
-}
-
-})
-
-
-
-
-router.get('/', function(req, res, next) {
-  request({
-    uri: 'https://api.printful.com',
-
-    function(error, response, body) {
-      if (!error && response.statusCode === 200) {
-        console.log(body);
-        res.json(body);
-      } else {
-        res.json(error);
-      }
-    }
-  });
+      currency: "USD",
+      description: "Forever Immortal",
+      payment_method: id,
+      confirm: true,
+    });
+    console.log(payment);
+    return res.status(200).json({ confirm: "done" });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({ message: error.message });
+  }
 });
 
-router.get('/store', function(req, res, next) {
-  request({
-    uri: 'https://api.printful.com/store',
-  
-    function(error, response, body) {
-      if (!error && response.statusCode === 200) {
-        console.log(body);
-        res.json(body);
-      } else {
-        res.json(error);
-      }
-    }
-  });
+server.get("/", async (request, response) => {
+  console.log(request.params);
+
+  const url = `https://api.printful.com/`;
+  const res = await fetch(url);
+  const data = await res.json();
+
+  response.json(data);
 });
 
-router.get('/store/products', function(req, res, next) {
-  request({
-    uri: 'https://api.printful.com/store/products',
-  
-    function(error, response, body) {
-      if (!error && response.statusCode === 200) {
-        console.log(body);
-        res.json(body);
-      } else {
-        res.json(error);
-      }
-    }
-  });
+server.get("/store", async (request, response) => {
+  console.log(request.params);
+
+  const url = `https://api.printful.com/store`;
+  const res = await fetch(url);
+  const data = await res.json();
+
+  response.json(data);
 });
+
+server.get("/store/products", async (request, response) => {
+  console.log(request.params);
+
+  const url = `https://api.printful.com/store/products`;
+  const res = await fetch(url);
+  const data = await res.json();
+
+  response.json(data);
+});
+
 module.exports = server;
